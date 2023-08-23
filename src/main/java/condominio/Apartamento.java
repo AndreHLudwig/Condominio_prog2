3014c0cd111b3de312c8222d50235d7965085055
Vitor André Gehrke <vitor.gehrke@gmail.com>
2023-08-22 21:59:24 -0300

    Calculadora de aluguel
    Definido método de calculadora de aluguel (+ variáveis, getters e setters necessários)

--- src/main/java/condominio/Apartamento.java
+++ src/main/java/condominio/Apartamento.java
@@ -14,4 +14,44 @@ public class Apartamento {
     private double metragem;
     private int vagasDeGaragem;
     private double valorCondominio;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Locatario getLocatario() {
+        return locatario;
+    }
+
+    public void setLocatario(Locatario locatario) {
+        this.locatario = locatario;
+    }
+
+    public double getMetragem() {
+        return metragem;
+    }
+
+    public void setMetragem(double metragem) {
+        this.metragem = metragem;
+    }
+
+    public int getVagasDeGaragem() {
+        return vagasDeGaragem;
+    }
+
+    public void setVagasDeGaragem(int vagasDeGaragem) {
+        this.vagasDeGaragem = vagasDeGaragem;
+    }
+
+    public double getValorCondominio() {
+        return valorCondominio;
+    }
+
+    public void setValorCondominio(double valorCondominio) {
+        this.valorCondominio = valorCondominio;
+    }
 }
